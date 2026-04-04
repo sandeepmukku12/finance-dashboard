6711d6157edd107f6c51a8a086a5867883318a70
sandeepmukku <sandeep12akp@gmail.com>
2026-04-05 02:12:05 +0530

    feat: add hover scale effect in summary card component

--- src/components/SummaryCard.jsx
+++ src/components/SummaryCard.jsx
@@ -1,18 +1,10 @@
 import clsx from "clsx";
 
-export default function SummaryCard({
-  title,
-  amount,
-  change,
-  isPositive,
-}) {
+export default function SummaryCard({ title, amount, change, isPositive }) {
   return (
-    <div className="bg-white dark:bg-[#111827] p-5 rounded-xl border border-gray-200 dark:border-gray-800 shadow-sm hover:shadow-md transition">
-      
+    <div className="bg-white dark:bg-[#111827] p-5 rounded-xl border border-gray-200 dark:border-gray-800 shadow-sm hover:shadow-md hover:scale-[1.02] transition">
       {/* Title */}
-      <p className="text-sm text-gray-500 dark:text-gray-400">
-        {title}
-      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-400">{title}</p>
 
       {/* Amount */}
       <h2 className="text-2xl font-semibold mt-2 text-gray-900 dark:text-white">
@@ -24,7 +16,7 @@ export default function SummaryCard({
         <span
           className={clsx(
             "text-sm font-medium",
-            isPositive ? "text-green-500" : "text-red-500"
+            isPositive ? "text-green-500" : "text-red-500",
           )}
         >
           {isPositive ? "↑" : "↓"} {change}%
