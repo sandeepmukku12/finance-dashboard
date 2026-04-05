39aa97d75b5fbe1df369c60124bf06e481cac6c2
sandeepmukku <sandeep12akp@gmail.com>
2026-04-06 00:28:24 +0530

    feat: update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import Dashboard from "./pages/Dashboard";
 import Layout from "./components/Layout";
+import TransactionsPage from "./pages/TransactionsPage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
+  const [activeTab, setActiveTab] = useState("dashboard");
+
   return (
-    <Layout>
-      <Dashboard />
+    <Layout activeTab={activeTab} setActiveTab={setActiveTab}>
+      {activeTab === "dashboard" && <Dashboard />}
+      {activeTab === "transactions" && <TransactionsPage />}
+      {activeTab === "settings" && <SettingsPage />}
     </Layout>
   );
 }
