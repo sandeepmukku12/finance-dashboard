34c68eebba0761a8af07b986b65b030c92c8895f
sandeepmukku <sandeep12akp@gmail.com>
2026-04-05 23:33:27 +0530

    feat: update dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,20 +4,39 @@ import ExpenseChart from "../components/ExpenseChart";
 import { balanceData, expenseData } from "../data/mockData";
 import TransactionTable from "../components/TransactionTable";
 import { transactions } from "../data/mockData";
+import { useApp } from "../context/AppContext";
+import { transactions as mockTx } from "../data/mockData";
 
 export default function Dashboard() {
+  const { transactions, setTransactions } = useApp();
+
+  useEffect(() => {
+    if (transactions.length === 0) {
+      setTransactions(mockTx);
+    }
+  }, []);
+
   return (
     <div className="p-6 space-y-6">
-
       <h1 className="text-2xl font-semibold text-gray-900 dark:text-white">
         Finance Dashboard
       </h1>
 
       {/* Summary Cards */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <SummaryCard title="Total Balance" amount={52340} change={12} isPositive />
+        <SummaryCard
+          title="Total Balance"
+          amount={52340}
+          change={12}
+          isPositive
+        />
         <SummaryCard title="Income" amount={84500} change={8} isPositive />
-        <SummaryCard title="Expenses" amount={32160} change={5} isPositive={false} />
+        <SummaryCard
+          title="Expenses"
+          amount={32160}
+          change={5}
+          isPositive={false}
+        />
       </div>
 
       {/* Charts */}
@@ -25,8 +44,6 @@ export default function Dashboard() {
         <BalanceChart data={balanceData} />
         <ExpenseChart data={expenseData} />
       </div>
-
-
 
       <TransactionTable data={transactions} />
     </div>
