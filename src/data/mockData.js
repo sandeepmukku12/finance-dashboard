22aa5a2e9b534d91dffa05d055939b54adb34993
sandeepmukku <sandeep12akp@gmail.com>
2026-04-05 23:04:08 +0530

    feat: create mock data

---
+++ src/data/mockData.js
@@ -0,0 +1,16 @@
+export const balanceData = [
+  { date: "Jan", balance: 20000 },
+  { date: "Feb", balance: 25000 },
+  { date: "Mar", balance: 22000 },
+  { date: "Apr", balance: 30000 },
+  { date: "May", balance: 35000 },
+  { date: "Jun", balance: 52340 },
+];
+
+export const expenseData = [
+  { name: "Food", value: 8000 },
+  { name: "Rent", value: 15000 },
+  { name: "Travel", value: 4000 },
+  { name: "Shopping", value: 3000 },
+  { name: "Other", value: 2160 },
+];
